57a110f0234ab21ac641d1fec70162882fe4aa42
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-01 15:57:58 -0600

    added necessary functionality for stat tracker class to pass initial test
    Co-Authored-by: Billy Urrutia <billyurrutia@gmail.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,6 +1,6 @@
-require './test_helper'
+require_relative 'test_helper'
 
-class StatTracker < MiniTest::Test
+class StatTrackerTest < MiniTest::Test
 
   def setup
     @stat_tracker = StatTracker.new
@@ -9,7 +9,5 @@ class StatTracker < MiniTest::Test
   def test_it_exists
     assert_instance_of StatTracker, @stat_tracker
   end
-
-
 
 end
