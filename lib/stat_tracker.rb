b12600b8149c7b05bb7cee30d1e50f4b6c421e9e
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-01 15:57:58 -0600

    added test for initialize for stat tracker
    Co-Authored-by: Billy Urrutia <billyurrutia@gmail.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,5 @@
 class StatTracker
 
+
+
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -10,4 +10,10 @@ class StatTrackerTest < MiniTest::Test
     assert_instance_of StatTracker, @stat_tracker
   end
 
+  def test_it_initiates_with_no_games_teams_or_stats
+    assert_equal [], @stat_tracker.games
+    assert_equal [], @stat_tracker.teams
+    assert_equal [], @stat_tracker.stats
+  end
+
 end
