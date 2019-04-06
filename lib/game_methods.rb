0bf338f4ff40486f756c7cd65585ad6fa73d7060
billy urrutia <billyurrutia@gmail.com>
2019-04-05 18:31:08 -0600

    3 game_methods were made and they test perfectly
    Co-Author-By: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- lib/game_methods.rb
+++ lib/game_methods.rb
@@ -1,4 +1,23 @@
 module GameMethods
-  
+
+  def highest_total_score
+    total = @games.max_by{|game| game.away_goals + game.home_goals}
+    total.away_goals + total.home_goals
+  end
+
+  def lowest_total_score
+    total = @games.min_by{|game| game.away_goals + game.home_goals}
+    total.away_goals + total.home_goals
+  end
+
+  def biggest_blowout
+    total = @games.max_by{|game| (game.away_goals - game.home_goals).abs}
+    (total.away_goals - total.home_goals).abs
+  end
+# percentage_home_wins	Percentage of games that a home team has won (rounded to the nearest 100th)	Float
+# percentage_visitor_wins	Percentage of games that a visitor has won (rounded to the nearest 100th)	Float
+# count_of_games_by_season	A hash with season names (e.g. 20122013) as keys and counts of games as values	Hash
+# average_goals_per_game	Average number of goals scored in a game across all seasons including both home and away goals (rounded to the nearest 100th)	Float
+# average_goals_by_season	Average number of goals scored in a game organized in a hash with season names (e.g. 20122013) as keys and a float representing the average number of goals in a game for that season as a key (rounded to the nearest 100th)	Hash
 
 end
--- test/stat_tracker_game_stat_test.rb
+++ test/stat_tracker_game_stat_test.rb
@@ -21,34 +21,39 @@ class StatTrackerGameStatTest < MiniTest::Test
   end
 
   def test_it_can_calculate_highest_total_score
-    assert_equal nil, @stat_tracker.highest_total_score
+    assert_equal 15 , @stat_tracker.highest_total_score
   end
 
   def test_it_can_calculate_lowest_total_score
-    assert_equal nil, @stat_tracker.lowest_total_score
+    assert_equal 2, @stat_tracker.lowest_total_score
   end
 
   def test_it_can_calculate_biggest_blowout
-    assert_equal nil, @stat_tracker.biggest_blowout
+    assert_equal 6, @stat_tracker.biggest_blowout
   end
 
   def test_it_can_calculate_percentage_home_wins
+    skip
     assert_equal nil, @stat_tracker.percentage_home_wins
   end
 
   def test_it_can_calculate_percentage_visitor_wins
+    skip
     assert_equal nil, @stat_tracker.percentage_visitor_wins
   end
 
   def test_it_can_calculate_count_of_games_by_season
+    skip
     assert_equal nil, @stat_tracker.count_of_games_by_season
   end
 
   def test_it_can_calculate_average_goals_per_game
+    skip
     assert_equal nil, @stat_tracker.average_goals_per_game
   end
 
   def test_it_can_calculate_average_goals_per_season
+    skip
     assert_equal nil, @stat_tracker.average_goals_by_season
   end
 
