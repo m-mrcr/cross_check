2b9799352066ba5c8a1cae8b689f84f57818c45a
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-08 20:43:41 -0600

    added best_offense method and accompanying helper methods
    Co-Authored-by: Billy Urrutia <billyurrutia@gmail.com>

--- lib/league_methods.rb
+++ lib/league_methods.rb
@@ -4,6 +4,29 @@ module LeagueMethods
     @teams.count
   end
 
+  def best_offense
+    best_team = average_goals_per_team.max_by{|x,y| y }
+    teamnames(best_team[0])
+  end
+
+  #---
+
+  def group_by_team
+    @stats.group_by {|game| game.team_id}
+  end
+
+  def average_goals_per_team
+    average = Hash.new
+    group_by_team.each do |team|
+      totals = []
+      team[1].each do |game|
+        totals << game.goals
+      end
+      average[team[0].to_s] = (totals.sum / totals.count.to_f).round(2)
+    end
+    average
+  end
+
   
 
 end
