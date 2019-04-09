ac7bab5f911758f153dce8055b700e6776808479
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-04-08 18:41:24 -0600

    added best_season method + helper methods
    Co-Authored-by: Billy Urrutia <billyurrutia@gmail.com>

--- lib/team_methods.rb
+++ lib/team_methods.rb
@@ -12,4 +12,28 @@ module TeamMethods
     return info
   end
 
+  def best_season(input)
+    final = percent_of_wins_by_season(input).max_by{|season, percent| percent}
+    final[0]
+  end
+
+  def percent_of_wins_by_season(input)
+    percent_of_wins_by_season = Hash.new
+    count_of_wins_by_season(input).map{|season, game|
+      percent_of_wins_by_season[season] =
+      (game.count.to_f/count_of_games_by_season[season]).round(2)}
+    percent_of_wins_by_season
+  end
+
+  def count_of_wins_by_season(input)
+    games_won(input).group_by{|game| game.season}
+  end
+
+  def count_of_games_by_season(input)
+    count_by_season = Hash.new
+    games_by_season(input).map do |season, games|
+      count_by_season[season] = games.count
+    end
+  end
+
 end
